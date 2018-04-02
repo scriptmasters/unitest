188464584bafc52ec5c3bc00fc78d316b1e76663
Melnyk Victoria <melnykvictoria21@gmail.com>
2018-04-02 13:01:51 +0300

    Revert "Add TypeScript file to Timetable component"
    This reverts commit 2990f7905a769ae81c2429dc5461e524db9fc12c.

--- src/app/admin/timetable/timetable.component.ts
+++ src/app/admin/timetable/timetable.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import TableService, { Subject, Group, TimeEntity } from "./timetable.service";
+
+interface TimeEntityTable extends TimeEntity {
+  subject_name: string;
+  group_name: string;
+}
 
 @Component({
-  selector: 'app-timetable',
-  templateUrl: './timetable.component.html',
-  styleUrls: ['./timetable.component.scss']
+  selector: "app-timetable",
+  templateUrl: "./timetable.component.html",
+  styleUrls: ["./timetable.component.css"],
+  providers: [TableService]
 })
 export class TimetableComponent implements OnInit {
+  table: TimeEntityTable[];
+  subjects: Map<string, string>;
+  groups: Map<string, string>;
 
-  constructor() { }
+  constructor(public tableService: TableService) {
+    this.subjects = new Map();
+    this.groups = new Map();
+    this.table = [];
 
-  ngOnInit() {
+    tableService.getTable().subscribe(
+      async data => {
+        await tableService
+          .getGroups()
+          .toPromise()
+          .then(groups => {
+            groups.forEach(group =>
+              this.groups.set(group.group_id, group.group_name)
+            );
+          });
+
+        await tableService
+          .getSubjects()
+          .toPromise()
+          .then(subjects => {
+            subjects.forEach(subject =>
+              this.subjects.set(subject.subject_id, subject.subject_name)
+            );
+          });
+
+        this.table = data.map((timeEntity: TimeEntity): TimeEntityTable => {
+          return Object.assign({}, timeEntity, {
+            group_name: this.groups.get(timeEntity.group_id),
+            subject_name: this.subjects.get(timeEntity.subject_id)
+          });
+        });
+
+        console.log(this.table);
+      },
+      err => {
+        this.table = [];
+      }
+    );
+
+    // this.getSubjects().forEach(subject =>
+    //   this.subjects.set(subject.subject_id, subject.subject_name)
+    // );
+
+    // this.getGroups().forEach(group =>
+    //   this.groups.set(group.group_id, group.group_name)
+    // );
+
+    // this.table = this.getTable().map(timeEntity => {
+    //   timeEntity.group_name = this.groups.get(timeEntity.group_id);
+    //   timeEntity.subject_name = this.subjects.get(timeEntity.subject_id);
+    //   return timeEntity;
+    // });
   }
 
+  ngOnInit() {}
 }
