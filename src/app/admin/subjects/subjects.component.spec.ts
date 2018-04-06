ab59014779aa9e35c6402935d18c8bd48070ec59
yuradanyshchuk <37478765+yuradanyshchuk@users.noreply.github.com>
2018-04-06 14:28:18 +0300

    Revert "CRUD Subjects"

--- src/app/admin/subjects/subjects.component.spec.ts
+++ src/app/admin/subjects/subjects.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubjectsComponent } from './subjects.component';
-import { MatDialog } from '@angular/material';
 
 describe('SubjectsComponent', () => {
   let component: SubjectsComponent;
--- src/app/admin/subjects/subjects.component.ts
+++ src/app/admin/subjects/subjects.component.ts
@@ -1,16 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material';
-
-import { SubjectService } from './services/subject.service';
-import {AddSubjectComponent} from './add-subject/add-subject.component';
-import {EditSubjectComponent} from './edit-subject/edit-subject.component';
-
-interface Subjects {
-  subject_id: number;
-  subject_name: string;
-  subject_description: string;
-}
 
 @Component({
   selector: 'app-subjects',
@@ -19,35 +7,9 @@ interface Subjects {
 })
 export class SubjectsComponent implements OnInit {
 
-  subjects: Subjects;
-  form: FormGroup;
-
-  constructor(
-    private subjectService: SubjectService,
-    public dialog: MatDialog
-  ) { }
+  constructor() { }
 
   ngOnInit() {
-    this.subjectService.getSubjects()
-      .subscribe((data: Subjects) => {
-        this.subjects = data;
-      });
-  }
-
-  openModalAdd() {
-    this.dialog.open(AddSubjectComponent, {
-      height: '400px',
-      width: '600px',
-      data: {name: 'test'}
-    });
-  }
-
-  openModalEdit(id) {
-    this.dialog.open(EditSubjectComponent, {
-      height: '400px',
-      width: '600px',
-      data: {subject_id: id, name: 'test'}
-    });
   }
 
 }
