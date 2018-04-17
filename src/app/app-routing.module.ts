c134817dbed13c4d8d85ddc70a4be113364e53ec
brylkovskyi <brylkovsky@gmail.com>
2018-04-17 20:14:29 +0300

    lazy loading admin auth and student modules added generated shared module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,81 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StudentComponent } from './student/student.component';
-import { AdminComponent } from './admin/admin.component';
-import { AuthComponent } from './auth/auth.component';
 import { StudentGuard } from './student-guard.service';
 import { AdminGuard } from './admin-guard.service';
-import { FacultiesComponent } from './admin/faculties/faculties.component';
-import { TestsComponent } from './admin/subjects/tests/tests.component';
-import { StatisticComponent } from './admin/statistic/statistic.component';
-import { SubjectsComponent } from './admin/subjects/subjects.component';
-import { SpecialitiesComponent } from './admin/specialities/specialities.component';
-import { AdministratorsComponent } from './admin/administrators/administrators.component';
-import { StudentsComponent } from './admin/students/students.component';
-import { GroupsComponent } from './admin/groups/groups.component';
-import { TimetableComponent } from './admin/timetable/timetable.component';
-import { QuestionsComponent } from './admin/questions/questions.component';
-import { TestDetailsComponent} from './admin/testdetails/component/test-details.component';
+
+
 
 
 const routes: Routes = [
     { path: '', redirectTo: '/admin/statistic', pathMatch: 'full' },
-    { path: 'student', canActivate: [StudentGuard], component: StudentComponent },
-    { path: 'login', component: AuthComponent },
-    {
-        path: 'admin',
-        component: AdminComponent,
-        canActivate: [AdminGuard],
-        children: [
-          {
-            path: 'statistic',
-            component: StatisticComponent
-          },
-          {
-            path: 'groups',
-            component: GroupsComponent
-          },
-          {
-            path: 'students',
-            component: StudentsComponent
-          },
-          {
-            path: 'students/byGroup/:id',
-            component: StudentsComponent
-          },
-          {
-            path: 'faculties',
-            component: FacultiesComponent
-          },
-          {
-            path: 'subjects',
-            component: SubjectsComponent
-          },
-          {
-            path: 'specialities',
-            component: SpecialitiesComponent
-          },      {
-            path: 'administrators',
-            component: AdministratorsComponent
-          },
-          {
-            path: 'timetable',
-            component: TimetableComponent
-          },
-          {
-            path: 'tests',
-            component: TestsComponent
-          },
-          {
-            path: 'questions',
-            component: QuestionsComponent
-          },
-          {
-            path: 'testdetails',
-            component: TestDetailsComponent
-          }
-        ]
-    }
+    { path: 'student', canActivate: [StudentGuard], loadChildren: './student/student.module#StudentModule' },
+    { path: 'login', loadChildren: './auth/auth.module#AuthModule' },
+    { path: 'admin', canActivate: [AdminGuard], loadChildren: './admin/admin.module#AdminModule'}
     // { path: '**', redirectTo: '/admin', pathMatch: 'full' }
 ];
 
