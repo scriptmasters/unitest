fc33c0c649196905f8723b57fb289ca206f24313
wingo7 <ihor.cherepaha@gmail.com>
2018-04-26 00:55:02 +0300

    'fix'

--- src/app/admin/tests/edit/edit.component.ts
+++ src/app/admin/tests/edit/edit.component.ts
@@ -2,7 +2,6 @@ import { Component, Inject, OnInit} from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material';
 import {TestService } from '../test.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import {NgClass} from '@angular/common';
 import { ResponseMessageComponent } from '../../../shared/response-message/response-message.component';
 import {forbiddenCharValidator} from '../tests-validator.directive';
 
