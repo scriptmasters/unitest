a86e9f7565bee5cd6603fa2386c176db6444334a
brylkovskyi <brylkovsky@gmail.com>
2018-04-30 13:55:28 +0300

    added search filter to subjects, deleted viewencapsulation.none inside students.component

--- src/app/admin/students/students-modal-window/students-modal-window.component.ts
+++ src/app/admin/students/students-modal-window/students-modal-window.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, Inject, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, Inject } from '@angular/core';
 import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 import { StudentsService } from '../students.service';
@@ -19,7 +19,6 @@ import { setGroupAsID } from '../reusable-functions/set-group-as-id';
   selector: 'app-students-modal-window',
   templateUrl: './students-modal-window.component.html',
   styleUrls: ['./students-modal-window.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class StudentsModalWindowComponent implements OnInit {
 
