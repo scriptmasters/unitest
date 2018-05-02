085c22e8ae6b854e911a57f285c244a203cb1940
Melnyk Victoria <melnykvictoria21@gmail.com>
2018-05-02 18:51:12 +0300

    fix breadcrumbs

--- src/app/shared/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -1,47 +1,46 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Router, NavigationEnd, ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
-// import { RoutePartsService } from './breadcrumb.service';
-// import 'rxjs/add/operator/filter';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterModule } from '@angular/router';
+import { BreadCrumb } from './breadcrumb';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/distinctUntilChanged';
 
-// @Component({
-//     selector: 'app-breadcrumb',
-//     templateUrl: './breadcrumb.component.html',
-//     styleUrls: [ './breadcrumb.component.scss' ],
-// })
-// export class BreadcrumbComponent implements OnInit {
-//     routeParts:any[];
-//   constructor(
-//     private router: Router,
-//     private routePartsService: RoutePartsService, 
-//     private activeRoute: ActivatedRoute
-//   ) {
-//     this.router.events.filter(event => event instanceof NavigationEnd).subscribe((routeChange) => {
-//       this.routeParts = this.routePartsService.generateRouteParts(this.activeRoute.snapshot);
-//       // generate url from parts
-//       this.routeParts.reverse().map((item, i) => {
-//         item.breadcrumb = this.parseText(item);
-//         item.urlSegments.forEach((urlSegment, j) => {
-//           if(j === 0)
-//             return item.url = `${urlSegment.path}`;
-//           item.url += `/${urlSegment.path}`
-//         });
-//         if(i === 0) {
-//           return item;
-//         }
-//         // prepend previous part to current part
-//         item.url = `${this.routeParts[i - 1].url}/${item.url}`;
-//         return item;
-//       });
-//     });
-//   }
+@Component({
+    selector: 'app-breadcrumb',
+    templateUrl: './breadcrumb.component.html',
+    styleUrls: [ './breadcrumb.component.scss' ],
+    encapsulation: ViewEncapsulation.None
+})
+export class BreadcrumbComponent implements OnInit {
+    breadcrumbs = this.router.events
+        .filter(event => event instanceof NavigationEnd)
+        .distinctUntilChanged()
+        .map(event => this.buildBreadCrumb(this.activatedRoute.root));
+    // Build your breadcrumb starting with the root route of your current activated route
+    constructor(private activatedRoute: ActivatedRoute,
+                private router: Router) {
+    }
 
-//   ngOnInit() {}
+    ngOnInit() {
+    }
 
-//   parseText(part) {
-//     part.breadcrumb = part.breadcrumb.replace(/{{([^{}]*)}}/g, function (a, b) {
-//       var r = part.params[b];
-//       return typeof r === 'string' ? r : a;
-//     });
-//     return part.breadcrumb;
-//   }
-// }
+    buildBreadCrumb(route: ActivatedRoute, url: string = '',
+                    breadcrumbs: Array<BreadCrumb> = []): Array<BreadCrumb> {
+        // If no routeConfig is avalailable we are on the root path
+        const label = route.routeConfig ? route.routeConfig.data[ 'breadcrumb' ] : 'Home';
+        const path = route.routeConfig ? route.routeConfig.path : '';
+        // In the routeConfig the complete path is not available,
+        // so we rebuild it each time
+        const nextUrl = `${url}${path}/`;
+        const breadcrumb = {
+            label: label,
+            url: nextUrl
+        };
+        const newBreadcrumbs = [ ...breadcrumbs, breadcrumb ];
+        if (route.firstChild) {
+            // If we are not on our current path yet,
+            // there will be more children to look after, to build our breadcumb
+            return this.buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
+        }
+        return newBreadcrumbs;
+    }
+}
