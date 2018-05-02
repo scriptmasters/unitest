c109895fd90f020d16acad383b7c9056cb29be5c
Melnyk Victoria <melnykvictoria21@gmail.com>
2018-05-02 12:30:24 +0300

    fix button in modal-subject component

---
+++ src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,47 @@
+// import { Component, OnInit } from '@angular/core';
+// import { Router, NavigationEnd, ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
+// import { RoutePartsService } from './breadcrumb.service';
+// import 'rxjs/add/operator/filter';
+
+// @Component({
+//     selector: 'app-breadcrumb',
+//     templateUrl: './breadcrumb.component.html',
+//     styleUrls: [ './breadcrumb.component.scss' ],
+// })
+// export class BreadcrumbComponent implements OnInit {
+//     routeParts:any[];
+//   constructor(
+//     private router: Router,
+//     private routePartsService: RoutePartsService, 
+//     private activeRoute: ActivatedRoute
+//   ) {
+//     this.router.events.filter(event => event instanceof NavigationEnd).subscribe((routeChange) => {
+//       this.routeParts = this.routePartsService.generateRouteParts(this.activeRoute.snapshot);
+//       // generate url from parts
+//       this.routeParts.reverse().map((item, i) => {
+//         item.breadcrumb = this.parseText(item);
+//         item.urlSegments.forEach((urlSegment, j) => {
+//           if(j === 0)
+//             return item.url = `${urlSegment.path}`;
+//           item.url += `/${urlSegment.path}`
+//         });
+//         if(i === 0) {
+//           return item;
+//         }
+//         // prepend previous part to current part
+//         item.url = `${this.routeParts[i - 1].url}/${item.url}`;
+//         return item;
+//       });
+//     });
+//   }
+
+//   ngOnInit() {}
+
+//   parseText(part) {
+//     part.breadcrumb = part.breadcrumb.replace(/{{([^{}]*)}}/g, function (a, b) {
+//       var r = part.params[b];
+//       return typeof r === 'string' ? r : a;
+//     });
+//     return part.breadcrumb;
+//   }
+// }
