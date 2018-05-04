cbed1c8f4a882ba4cbb3155742904b8d07b26318
Yura <yuradanyshchuk@gmail.com>
2018-05-04 14:31:06 +0300

    testPlayer update test

--- src/app/student/test-player/test-player.component.ts
+++ src/app/student/test-player/test-player.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import { TestPlayerService } from '../services/test-player.service';
-import { ActivatedRoute } from '@angular/router';
-import { AuthService } from '../../auth/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import {MatDialog} from '@angular/material';
+import {TestResultComponent} from './test-result/test-result.component';
 
 @Component({
   selector: 'app-test-player',
@@ -12,11 +13,13 @@ import { AuthService } from '../../auth/auth.service';
 export class TestPlayerComponent implements OnInit {
   questions = [];
   userAnswers = {};
-  selectedAnswers = {}; // for checkbox question
+  userCheckboxAnswers = {}; // for checkbox question
 
   constructor(
-    public authService: AuthService,
-    private testPlayerService: TestPlayerService
+    private testPlayerService: TestPlayerService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private dialog: MatDialog
   ) {}
 
   ngOnInit() {
@@ -24,33 +27,53 @@ export class TestPlayerComponent implements OnInit {
   }
 
   getQuestionsForTest(): void {
+    const testId = +this.route.snapshot.paramMap.get('id');
     this.testPlayerService
-      .getQuestionsWithAnswers()
+      .getQuestionsWithAnswers(testId)
       .subscribe((questions: any) => {
         this.questions = questions;
       });
   }
 
-  selectedAnswer(question_id: number, answer) {
-    this.userAnswers[answer.answer_id] = answer;
-
-    if (this.selectedAnswers[answer.answer_id] === false) {
-      delete this.userAnswers[answer.answer_id];
-    } else {
-      this.userAnswers[answer.answer_id].answer_id = [answer.answer_id];
+  selectedAnswer(question, answer) {
+    this.userCheckboxAnswers[question.question_id] = this.userCheckboxAnswers[question.question_id] || {};
+    const topModel = this.userCheckboxAnswers[question.question_id];
+    topModel[answer.answer_id] = !topModel[answer.answer_id];
+    const answersArr = [];
+    let answers_ids = '';
+    for (const key in topModel) {
+      if (topModel[key] === false) {
+        delete topModel[key];
+      } else {
+        answersArr.push(key);
+        answers_ids = answersArr.join(',');
+      }
     }
+
+    this.userAnswers[question.question_id] = this.userAnswers[question.question_id] || {};
+    this.userAnswers[question.question_id].question_id = question.question_id;
+    this.userAnswers[question.question_id].answer_id = answers_ids;
+
+  }
+
+  openModal(testResult): void {
+    const matDialogRef = this.dialog.open(TestResultComponent, {
+      disableClose: true,
+      width: '400px',
+      data: { result: testResult }
+    });
+
+    matDialogRef.afterClosed().subscribe(() => {
+      this.router.navigate(['student']);
+    });
   }
 
   finishTest() {
-    // console.log(this.userAnswers);
     console.log('Finish Test');
-    this.testPlayerService.checkResult(this.userAnswers);
-    // this.testPlayerService.checkResult(this.userAnswers)
-    //   .subscribe((response: any) => {
-    //     console.log(response);
-    //   });
+    this.testPlayerService.checkResult(this.userAnswers)
+      .subscribe((response: any) => {
+        const testResult = response;
+        this.openModal(testResult);
+      });
   }
-
-  // ***** TIMER *****
-  a = 'TIMER';
 }
