65c9636c8cd6010cb0fe018750a3fa0cef695eb0
wingo7 <ihor.cherepaha@gmail.com>
2018-05-13 00:17:47 +0300

    add header to quiz entity

--- src/app/student/test-player/quiz-result/quiz-result.component.ts
+++ src/app/student/test-player/quiz-result/quiz-result.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
+import { AuthService } from '../../../auth/auth.service';
 
 @Component({
   selector: 'app-quiz-result',
@@ -9,7 +10,7 @@ import { DataService } from '../../services/data.service';
 export class QuizResultComponent implements OnInit {
   mark;
   answers;
-  constructor(private data: DataService) { }
+  constructor(private data: DataService, public authService: AuthService) { }
   ngOnInit() {
     this.mark = this.data.getMark();
     this.answers = this.data.getAnswers();
